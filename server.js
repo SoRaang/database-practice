fc42335fd6dfbe2b5b14e35aaa7d786e89d80482
SoRaang <dsc0320@hotmail.com>
2024-08-07 11:48:18 +0900

    Update

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema({ // 데이터 스키마 설정
 
 const userModel = mongoose.model('users', userSchema); // users 테이블을 위의 userSchema로 해석
 
-app.get('/', async (req, res) => { // 지정된 API 주소로 데이터를 불러오기
+app.get('/users', async (req, res) => { // 지정된 API 주소로 데이터를 불러오기
     const userData = await userModel.find();
 
     res.json(userData);
